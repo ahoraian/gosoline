9bb05f8e8483dfa01b5202ea5eff5daad8e020ee
bogdanfinn <bogdan.finn@googlemail.com>
2020-10-09 10:52:08 +0200

    wip

--- pkg/stream/batch_consumer.go
+++ pkg/stream/batch_consumer.go
@@ -14,11 +14,12 @@ import (
 
 //go:generate mockery -name=BatchConsumerCallback
 type BatchConsumerCallback interface {
-	Boot(config cfg.Config, logger mon.Logger)
-	Consume(ctx context.Context, msg []*Message) error
+	Boot(config cfg.Config, logger mon.Logger) error
+	GetModel(attributes map[string]interface{}) interface{}
+	Consume(ctx context.Context, models []*Message) (bool, error)
 }
 
-type baseBatchConsumer struct {
+type BaseBatchConsumer struct {
 	kernel.EssentialModule
 	kernel.ApplicationStage
 
@@ -39,23 +40,40 @@ type baseBatchConsumer struct {
 	force     bool
 }
 
-func (c *baseBatchConsumer) Boot(config cfg.Config, logger mon.Logger) error {
-	c.logger = logger
-	c.tracer = tracing.ProviderTracer(config, logger)
-	c.cfn = coffin.New()
+func NewBatchConsumer(name string, callback BatchConsumerCallback) *BaseBatchConsumer {
+	return &BaseBatchConsumer{
+		name:     name,
+		callback: callback,
+	}
+}
 
+func (c *BaseBatchConsumer) Boot(config cfg.Config, logger mon.Logger) error {
+	if err := c.bootCallback(config, logger); err != nil {
+		return err
+	}
+
+	tracer := tracing.ProviderTracer(config, logger)
+	cfn := coffin.New()
+
+	batchSize := config.GetInt("consumer_batch_size")
 	idleTimeout := config.GetDuration("consumer_idle_timeout")
-	c.ticker = time.NewTicker(idleTimeout * time.Second)
+	ticker := time.NewTicker(idleTimeout * time.Second)
 
-	c.batchSize = config.GetInt("consumer_batch_size")
-	c.batch = make([]*Message, 0, c.batchSize)
-
-	c.callback.Boot(config, logger)
+	c.BootWithInterfaces(logger, tracer, cfn, batchSize, ticker)
 
 	return nil
 }
 
-func (c *baseBatchConsumer) Run(ctx context.Context) error {
+func (c *BaseBatchConsumer) BootWithInterfaces(logger mon.Logger, tracer tracing.Tracer, cfn coffin.Coffin, batchSize int, ticker *time.Ticker) {
+	c.logger = logger
+	c.tracer = tracer
+	c.cfn = cfn
+	c.batchSize = batchSize
+	c.batch = make([]*Message, 0, batchSize)
+	c.ticker = ticker
+}
+
+func (c *BaseBatchConsumer) Run(ctx context.Context) error {
 	defer c.logger.Info("leaving consumer ", c.name)
 
 	c.cfn.GoWithContextf(ctx, c.input.Run, "panic during run of the consumer input")
@@ -79,7 +97,7 @@ func (c *baseBatchConsumer) Run(ctx context.Context) error {
 	}
 }
 
-func (c *baseBatchConsumer) consume() error {
+func (c *BaseBatchConsumer) consume() error {
 	for {
 		msg, ok := <-c.input.Data()
 
@@ -99,16 +117,58 @@ func (c *baseBatchConsumer) consume() error {
 	}
 }
 
-func (c *baseBatchConsumer) consumeBatch() {
+func (c *BaseBatchConsumer) consumeBatch() {
 	if len(c.batch) == 0 {
 		return
 	}
 
+	ctx := context.Background()
+
 	c.m.Lock()
 	defer c.m.Unlock()
 
-	_ = c.callback.Consume(context.Background(), c.batch)
+	ack, err := c.callback.Consume(ctx, c.batch)
+
+	if err != nil {
+		c.logger.Error(err, "an error occurred during the consume operation")
+	}
+
+	if !ack {
+		return
+	}
+
+	c.AcknowledgeBatch(ctx, c.batch)
 
 	c.processed += len(c.batch)
 	c.batch = make([]*Message, 0, c.batchSize)
+}
+
+func (c *BaseBatchConsumer) bootCallback(config cfg.Config, logger mon.Logger) error {
+	loggerCallback := logger.WithChannel("callback")
+	contextEnforcingLogger := mon.NewContextEnforcingLogger(loggerCallback)
+
+	err := c.callback.Boot(config, contextEnforcingLogger)
+
+	if err != nil {
+		return fmt.Errorf("error during booting the consumer callback: %w", err)
+	}
+
+	contextEnforcingLogger.Enable()
+
+	return nil
+}
+
+func (c *BaseBatchConsumer) AcknowledgeBatch(ctx context.Context, msg []*Message) {
+	var ok bool
+	var ackInput AcknowledgeableInput
+
+	if ackInput, ok = c.input.(AcknowledgeableInput); !ok {
+		return
+	}
+
+	err := ackInput.AckBatch(msg)
+
+	if err != nil {
+		c.logger.WithContext(ctx).Error(err, "could not acknowledge the messages")
+	}
 }
